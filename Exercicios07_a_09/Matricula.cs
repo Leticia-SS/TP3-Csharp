dee0c1611c3f28006406cbe41f6b5d2f369fc6f9
Leticia-SS <leticia.saraiva013@gmail.com>
2025-03-22 16:51:51 -0300

    exercicios 07 a 09

---
+++ Exercicios07_a_09/Matricula.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios07_a_09
+{
+    internal class Matricula
+    {
+        private string _NomeDoAluno;
+        private string _Curso;
+        private int _NumeroMatricula;
+        private string _Situacao;
+        private string _DataInicial;
+
+        public Matricula(string nomeDoAluno, string curso, int numeroMatricula, string situacao, string dataInicial)
+        {
+            _NomeDoAluno = nomeDoAluno;
+            _Curso = curso;
+            _NumeroMatricula = numeroMatricula;
+            _Situacao = situacao;
+            _DataInicial = dataInicial;
+        }
+
+        public void Trancar()
+        {
+            _Situacao = "Trancada";
+        }
+
+        public void Reativar()
+        {
+            _Situacao = "Ativa"; 
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Aluno: {_NomeDoAluno}\nCurso: {_Curso}\nSituação Atual: {_Situacao}\nData Inicial: {_DataInicial}");    
+        }
+    }
+}
---
+++ Exercicios07_a_09/TesteMatricula.cs
@@ -0,0 +1,19 @@
+﻿using Exercicios07_a_09;
+
+class TesteMatricula
+{
+    public static void Main(string[] args)
+    {
+        Matricula aluno = new Matricula("Lara Saraiva", "Engenharia de Software", 123456, "Ativa", "29/06/2023");
+        aluno.ExibirInformacoes();
+
+        Console.WriteLine("\n"); // Pulando linha
+        aluno.Trancar();
+        aluno.ExibirInformacoes();
+
+        Console.WriteLine("\n"); // Pulando linha
+        aluno.Reativar();
+        aluno.ExibirInformacoes();
+
+    }
+}
